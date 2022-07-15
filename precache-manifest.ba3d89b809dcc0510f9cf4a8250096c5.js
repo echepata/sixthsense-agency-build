bf97e5e7f8b4ada4269e3d84b16f95b951896488
Juan Diego Echeverri <juaneche@msn.com>
2022-07-15 23:19:07 +1000

    Release v3.0

---
+++ precache-manifest.ba3d89b809dcc0510f9cf4a8250096c5.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7e9ec90669917a37666eee55ac691c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2d8b9338ccda6b555e",
+    "url": "/static/css/2.862b2f9f.chunk.css"
+  },
+  {
+    "revision": "f41a9e3279f3cd6036e2",
+    "url": "/static/css/main.af5203c7.chunk.css"
+  },
+  {
+    "revision": "ca2d8b9338ccda6b555e",
+    "url": "/static/js/2.e7a075c0.chunk.js"
+  },
+  {
+    "revision": "5e7a3bbd1766925a2d65977472adeaa7",
+    "url": "/static/js/2.e7a075c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41a9e3279f3cd6036e2",
+    "url": "/static/js/main.b7984179.chunk.js"
+  },
+  {
+    "revision": "ef9fd814e8a1eb0f8d52",
+    "url": "/static/js/runtime-main.3bcdf86e.js"
+  },
+  {
+    "revision": "9531f34a4c09c5d0774084788cad3b89",
+    "url": "/static/media/CesarImage.9531f34a.jpg"
+  },
+  {
+    "revision": "404a18a12cc75de8b95f6cf41309ca4d",
+    "url": "/static/media/bartender.404a18a1.jpg"
+  },
+  {
+    "revision": "19466f6c876ee001414b7c54dac19230",
+    "url": "/static/media/booking-partner-logo.19466f6c.png"
+  },
+  {
+    "revision": "3a6affd7724b47632b156395a68dd311",
+    "url": "/static/media/cocktail1.3a6affd7.jpg"
+  },
+  {
+    "revision": "55f340a678ca698e70dc91b779114922",
+    "url": "/static/media/cocktail2.55f340a6.jpg"
+  },
+  {
+    "revision": "88c08b08add233a6b0bf70ada83e0a0d",
+    "url": "/static/media/cocktail3.88c08b08.jpg"
+  },
+  {
+    "revision": "0046a52b5f1f43f5bad593955817d7ce",
+    "url": "/static/media/cocktail4.0046a52b.jpg"
+  },
+  {
+    "revision": "229b7a2689658bf54f17ccd9a28bb267",
+    "url": "/static/media/events-six1.229b7a26.jpg"
+  },
+  {
+    "revision": "78c283b8ba7e5f6fd9dcb12251c6826a",
+    "url": "/static/media/events-six2.78c283b8.jpg"
+  },
+  {
+    "revision": "60c07008020963cdff497462cfcf04c2",
+    "url": "/static/media/events-six3.60c07008.jpg"
+  },
+  {
+    "revision": "f214efa6694e9ed3817a401a976924bd",
+    "url": "/static/media/events-six4.f214efa6.jpg"
+  },
+  {
+    "revision": "9435933e7df7d1f4063bffdd8ade8e20",
+    "url": "/static/media/events-six5.9435933e.jpg"
+  },
+  {
+    "revision": "fb993a59a551988e2b797f0af93bda93",
+    "url": "/static/media/events-six6.fb993a59.jpg"
+  },
+  {
+    "revision": "3ecdfb12bcb5672a42764b80d61aadf8",
+    "url": "/static/media/experience-pic-1.3ecdfb12.jpg"
+  },
+  {
+    "revision": "11c15d0665a60c8c2e72b8be41df59ff",
+    "url": "/static/media/experience-pic-2.11c15d06.jpg"
+  },
+  {
+    "revision": "b5283408a778ab684d0dce7cd0921a9e",
+    "url": "/static/media/experience-pic-3.b5283408.jpg"
+  },
+  {
+    "revision": "75e8b41d19037d1587c70464b4771179",
+    "url": "/static/media/red-wine.75e8b41d.jpg"
+  },
+  {
+    "revision": "c53f8dcee83473152435e26a6193c628",
+    "url": "/static/media/sp-about.c53f8dce.jpg"
+  },
+  {
+    "revision": "f57c70562dbae7e54be3667d9a126ad4",
+    "url": "/static/media/sp-events.f57c7056.jpg"
+  },
+  {
+    "revision": "470980929a1034e33c46622591764130",
+    "url": "/static/media/sp-jobs.47098092.jpg"
+  },
+  {
+    "revision": "dc2fa3afa0040362b8da4bd4909c51e7",
+    "url": "/static/media/sp-memories.dc2fa3af.jpg"
+  },
+  {
+    "revision": "d4a0177c45268cd04c20e961ac1d460c",
+    "url": "/static/media/sp-staff.d4a0177c.jpg"
+  },
+  {
+    "revision": "d606ba05fe8c45562b080a32b52e8079",
+    "url": "/static/media/sp-staff.d606ba05.png"
+  },
+  {
+    "revision": "d83212c1bea7462ac418e08b2d0efee8",
+    "url": "/static/media/spinner.d83212c1.gif"
+  },
+  {
+    "revision": "166f349c6fc4a2edff9281d6e6be8b56",
+    "url": "/static/media/staff-six2.166f349c.jpg"
+  },
+  {
+    "revision": "5afd0cb5fd76055440a875a5ad4ecdb3",
+    "url": "/static/media/staff-six3.5afd0cb5.jpg"
+  },
+  {
+    "revision": "8b7e574adab673cc4862851989d1f5ab",
+    "url": "/static/media/staff-six4.8b7e574a.jpg"
+  },
+  {
+    "revision": "64e3c6406a511fc8ccfe8e9bc8ba463d",
+    "url": "/static/media/staff-six5.64e3c640.jpg"
+  },
+  {
+    "revision": "e2e088ea4b244f866e466859627dd8e5",
+    "url": "/static/media/staff-six6.e2e088ea.jpg"
+  },
+  {
+    "revision": "8bda59e0b577c6fb40c8318d7ccb864b",
+    "url": "/static/media/two-waiters.8bda59e0.jpg"
+  },
+  {
+    "revision": "0a5f57c4c3c3e550081e7bdffcb42ad0",
+    "url": "/static/media/waiter-champagne.0a5f57c4.jpg"
+  }
+]);
